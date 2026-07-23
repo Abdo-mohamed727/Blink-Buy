0d5258b0aae2739d7d20a867b0b9a9b9a0c6084f
Mariem Tayel <mariemtayel2@gmail.com>
2026-07-23 13:35:57 +0300

    editing

--- lib/core/model/item/product_item_entity.dart
+++ lib/core/model/item/product_item_entity.dart
@@ -1,76 +1,76 @@
 class ProductItemEntity {
-  int? id;
-  String? title;
-  String? description;
-  String? category;
-  double? price;
-  double? discountPercentage;
-  double? rating;
-  int? stock;
-  List<String>? tags;
-  String? brand;
-  String? sku;
-  int? weight;
-  DimensionsEntity? dimensions;
-  String? warrantyInformation;
-  String? shippingInformation;
-  String? availabilityStatus;
-  List<ReviewsEntity>? reviews;
-  String? returnPolicy;
-  int? minimumOrderQuantity;
-  MetaEntity? meta;
-  List<String>? images;
-  String? thumbnail;
+  int id;
+  String title;
+  String description;
+  String category;
+  double price;
+  double discountPercentage;
+  double rating;
+  int stock;
+  List<String> tags;
+  String brand;
+  String sku;
+  int weight;
+  DimensionsEntity dimensions;
+  String warrantyInformation;
+  String shippingInformation;
+  String availabilityStatus;
+  List<ReviewsEntity> reviews;
+  String returnPolicy;
+  int minimumOrderQuantity;
+  MetaEntity meta;
+  List<String> images;
+  String thumbnail;
 
 
   ProductItemEntity(
-      {this.id,
-        this.title,
-        this.description,
-        this.category,
-        this.price,
-        this.discountPercentage,
-        this.rating,
-        this.stock,
-        this.tags,
-        this.brand,
-        this.sku,
-        this.weight,
-        this.dimensions,
-        this.warrantyInformation,
-        this.shippingInformation,
-        this.availabilityStatus,
-        this.reviews,
-        this.returnPolicy,
-        this.minimumOrderQuantity,
-        this.meta,
-        this.images,
-        this.thumbnail});
+      {required this.id,
+        required this.title,
+        required this.description,
+        required this.category,
+        required this.price,
+        required this.discountPercentage,
+      required this.rating,
+        required this.stock,
+        required this.tags,
+        required this.brand,
+        required this.sku,
+        required this.weight,
+        required this.dimensions,
+        required this.warrantyInformation,
+        required this.shippingInformation,
+        required this.availabilityStatus,
+        required this.reviews,
+        required this.returnPolicy,
+        required this.minimumOrderQuantity,
+        required this.meta,
+        required this.images,
+        required this.thumbnail});
 
 }
 
 class DimensionsEntity {
-  double? width;
-  double? height;
-  double? depth;
+  double width;
+  double height;
+  double depth;
 
-  DimensionsEntity({this.width, this.height, this.depth});
+  DimensionsEntity({required this.width, required this.height, required this.depth});
 
 }
 
 class ReviewsEntity {
-  int? rating;
-  String? comment;
-  String? date;
-  String? reviewerName;
-  String? reviewerEmail;
+  int rating;
+  String comment;
+  String date;
+  String reviewerName;
+  String reviewerEmail;
 
   ReviewsEntity(
-      {this.rating,
-        this.comment,
-        this.date,
-        this.reviewerName,
-        this.reviewerEmail});
+      {required this.rating,
+        required this.comment,
+        required this.date,
+        required this.reviewerName,
+        required this.reviewerEmail});
 
 
 
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:blinkbuy/core/route/app_router.dart';
-import 'package:blinkbuy/core/route/app_routes.dart';
 import 'package:blinkbuy/core/comman/screens/check_network.dart';
 import 'package:blinkbuy/core/comman/widgets/connectivity_controller.dart';
 import 'package:flutter/material.dart';
