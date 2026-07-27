2e8153838655b479251c3818ab11de1ace50912c
Mariem Tayel <mariemtayel2@gmail.com>
2026-07-27 12:34:33 +0300

    edit ui product by category

---
+++ lib/features/products_ by_ category/presentation/view/widget/product_by_category_view_body.dart
@@ -0,0 +1,41 @@
+import 'package:blinkbuy/core/comman/widgets/product_card.dart';
+import 'package:blinkbuy/core/model/item/product_item_entity.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class ProductByCategoryViewBody extends StatelessWidget {
+  const ProductByCategoryViewBody({
+    super.key,
+    required this.products,
+    required this.crossAxisCount,
+    required this.childAspectRatio,
+  });
+
+  final List<ProductItemEntity> products;
+  final int crossAxisCount;
+  final double childAspectRatio;
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView.builder(
+      padding: EdgeInsets.fromLTRB(20.w, 0, 20.w, 12.h),
+      itemCount: products.length,
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: crossAxisCount,
+        crossAxisSpacing: 10.w,
+        mainAxisSpacing: 20.h,
+        childAspectRatio: childAspectRatio,
+      ),
+      itemBuilder: (context, index) {
+        return ProductCard(
+          productItemEntity: products[index],
+          imageUrl: products[index].images.first,
+          productName: products[index].title,
+          price: products[index].price,
+          discount: products[index].discountPercentage,
+          rating: products[index].rating,
+        );
+      },
+    );
+  }
+}
