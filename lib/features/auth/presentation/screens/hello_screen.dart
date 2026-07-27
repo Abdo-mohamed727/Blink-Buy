44c4c1491f88b9465781dba55dfff90636d74bee
WeSsam-Zakaria2 <wessamzakaria@fci.bu.edu.eg>
2026-07-27 14:49:40 +0300

    feat: update HelloScreen to use responsive sizing for padding and image dimensions

--- lib/features/auth/presentation/screens/hello_screen.dart
+++ lib/features/auth/presentation/screens/hello_screen.dart
@@ -2,6 +2,7 @@ import 'package:blinkbuy/core/comman/widgets/custom_button.dart';
 import 'package:blinkbuy/core/route/app_routes.dart';
 import 'package:blinkbuy/core/theme/app_colors.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class HelloScreen extends StatefulWidget {
   const HelloScreen({super.key});
@@ -16,21 +17,21 @@ class _HelloScreenState extends State<HelloScreen> {
     return Scaffold(
       backgroundColor: AppColors.backgroundColor,
       body: Padding(
-        padding: const EdgeInsets.all(16.0),
+        padding: EdgeInsets.all(16.w),
         child: Column(
           mainAxisAlignment: .center,
           spacing: 20,
           children: [
             Image(
               image: AssetImage("assets/images/hello.png"),
-              width: 343,
-              height: 261,
+              width: 343.w,
+              height: 261.h,
             ),
 
             Image(
               image: AssetImage("assets/images/hello_word.png"),
-              width: 147,
-              height: 74,
+              width: 147.w,
+              height: 74.h,
             ),
 
             CustomButton(
@@ -41,7 +42,7 @@ class _HelloScreenState extends State<HelloScreen> {
               },
               text: "Sign up",
               width: double.infinity,
-              height: 52,
+              height: 52.h,
               textAlign: TextAlign.center,
               textColor: Colors.white,
             ),
@@ -54,7 +55,7 @@ class _HelloScreenState extends State<HelloScreen> {
               text: "Sign In",
               textColor: Color(0xFFFF9900),
               width: double.infinity,
-              height: 52,
+              height: 52.h,
               backgroundColor: AppColors.backgroundColor,
             ),
           ],
